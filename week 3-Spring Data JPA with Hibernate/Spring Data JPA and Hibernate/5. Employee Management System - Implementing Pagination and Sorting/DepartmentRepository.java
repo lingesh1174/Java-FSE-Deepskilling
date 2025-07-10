f7fa4e67534888f150432135550217eb703c8cc8
lingesh1174 <147793730+lingesh1174@users.noreply.github.com>
2025-07-10 23:25:51 +0530

    Delete week 3-Spring Data JPA with Hibernate/Spring Data JPA and Hibernate/5. Employee Management System - Implementing Pagination and Sorting directory

--- week 3-Spring Data JPA with Hibernate/Spring Data JPA and Hibernate/5. Employee Management System - Implementing Pagination and Sorting/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.employee.repository;
-
-import com.cognizant.employee.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-	Department findByName(String name);
-}
